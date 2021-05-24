4cc8d22b620a53ed4d93a9c68b99faa9a1b20004
Shabu Uziel <shabi.uziel@gmail.com>
2021-05-24 21:59:36 +0300

    latest update 24-05-2021

--- tests/API_tests/test_update_task.py
+++ tests/API_tests/test_update_task.py
@@ -1,3 +1,5 @@
+import json
+
 import requests
 
 
@@ -14,29 +16,22 @@ if __name__ == '__main__':
     Step 2 - From Tasks - Retrieve Tadk_ID (Should be in data)
     '''
     url = "http://localhost:8081/api/v1/tasks"
-    task = {"taskDescription": "furnish the living room "}
+    task = {"taskDescription": "furnish the living room"}
     response = requests.get(url, json=task, headers=header)
     status = response.status_code
     data = response.json()
     '''
-    step 3 - access Task with task_ID and make the required changes)
+    step 3 - access Task with task_ID  from "data" field and make the required changes)
     '''
-    url = "http://localhost:8081/api/v1/task_id"
-    header = {"taskDescription": "furnish the living room ", "isCompleted": True}
-    task_ID = { "task_id": data[data][1].taskId}
-    response = requests.post(url,json=task_ID, headers=header)
+    TID = data['data'][1]['taskId']
+    url = "http://localhost:8081/api/v1/tasks/" + TID
+    payload = {"taskDescription": "furnish the living room",  "isCompleted": "True"}
+    response = requests.put(url,json=payload, headers=header)
     status = response.status_code
     data = response.json()
 
 
 
-    # status = response.status_code
-    # data = response.json()
-    # actual_url = response.url
-
-    # furnish the living room
-
-    print(status)
-    print(data)
-    # print(actual_url)
+    # print(status)
+    # print(data)
 
